d7e2ae3534c7ba893a2cab6ca34cfa17eb017d94
Jacob Traunero <jt7991@gmail.com>
2025-08-03 19:00:36 -0400

    Move things around

--- src/components/GameTable.tsx
+++ src/components/GameTable.tsx
@@ -19,115 +19,117 @@ export const GameCard = ({
 }) => {
   const [isCollapsed, setIsCollapsed] = useState(true);
   return (
-    <div className="bg-slate-700 shadow rounded-lg">
-      <div
-        className={`bg-slate-900 p-4 ${isCollapsed ? "rounded-lg" : "rounded-t-lg"} flex flex-row justify-between`}
-      >
-        <div className="flex flex-row gap-4">
-          <button
-            onClick={() => setIsCollapsed(!isCollapsed)}
-            className="flex flex-row text-sm items-center"
-          >
-            {isCollapsed ? <ChevronDownIcon /> : <ChevronUpIcon />}
-          </button>
-          <h2 className="font-bold text-2xl flex flex-row gap-4 items-center">
-            <div className="flex flex-row gap-0 items-center">
-              <TeamLogo teamId={game.awayTeam.name} />
-              {game.awayTeam.name}
-            </div>
-            at
-            <div className="flex flex-row gap-0 items-center">
-              <TeamLogo teamId={game.homeTeam.name} />
-              {game.homeTeam.name}
-            </div>
-          </h2>
-        </div>
-        <h2 className="font-bold text-sm">
-          {dayjs(game.date).format("h:mm A z")}
-        </h2>
-      </div>
-      <div
-        className={`grid xl:grid-cols-2 grid-cols-1 gap-4 p-2 ${isCollapsed ? "hidden" : ""}`}
-      >
-        <div>
-          <div
-            className="p-4 rounded-t-lg "
-            style={{
-              backgroundColor:
-                TEAM_COLORS?.[game.awayTeam.name as keyof typeof TEAM_COLORS]
-                  .primary,
-            }}
-          >
-            <h3
-              className={`text-xl font-bold flex justify-between items-center ${
-                TEAM_COLORS?.[game.awayTeam.name as keyof typeof TEAM_COLORS]
-                  .text
-              }`}
+    <div className="flex flex-col w-full">
+      <h2 className="font-bold text-sm p-2 py-0 pb-1">
+        {dayjs(game.date).format("h:mm A z")}
+      </h2>
+      <div className="bg-slate-700 shadow rounded-lg">
+        <div
+          className={`bg-slate-900 p-4 ${isCollapsed ? "rounded-lg" : "rounded-t-lg"} flex flex-col`}
+        >
+          <div className="flex flex-row gap-2">
+            <button
+              onClick={() => setIsCollapsed(!isCollapsed)}
+              className="flex flex-row text-sm items-center"
             >
-              <div
-                style={{ display: "flex", alignItems: "center", gap: "8px" }}
-              >
-                <TeamLogo teamId={game.awayTeam.name?.toLowerCase()} />
-                <span>{game.awayTeam.name}</span>
+              {isCollapsed ? <ChevronDownIcon /> : <ChevronUpIcon />}
+            </button>
+            <h2 className="font-bold text-lg flex flex-row gap-4 items-center">
+              <div className="flex flex-row gap-0 items-center">
+                <TeamLogo teamId={game.awayTeam.name} />
+                {game.awayTeam.name}
               </div>
-              <span
-                title={
-                  game.awayLineupConfirmed
-                    ? "Away lineup confirmed"
-                    : "Away lineup not confirmed"
-                }
-              >
-                {game.awayLineupConfirmed ? "✅" : "❌"}
-              </span>
-            </h3>
+              at
+              <div className="flex flex-row gap-0 items-center">
+                <TeamLogo teamId={game.homeTeam.name} />
+                {game.homeTeam.name}
+              </div>
+            </h2>
           </div>
-          <Suspense>
-            <LineupSection
-              teamId={game.awayTeam.id}
-              opposingTeamId={game.homeTeam.id}
-              gameId={game.id}
-            />
-          </Suspense>
         </div>
-        <div>
-          <div
-            className="p-4 rounded-t-lg "
-            style={{
-              backgroundColor:
-                TEAM_COLORS?.[game.homeTeam.name as keyof typeof TEAM_COLORS]
-                  .primary,
-            }}
-          >
-            <h3
-              className={`text-xl font-bold flex justify-between items-center ${
-                TEAM_COLORS?.[game.homeTeam.name as keyof typeof TEAM_COLORS]
-                  .text
-              }`}
+        <div
+          className={`grid xl:grid-cols-2 grid-cols-1 gap-4 p-2 ${isCollapsed ? "hidden" : ""}`}
+        >
+          <div>
+            <div
+              className="p-4 rounded-t-lg "
+              style={{
+                backgroundColor:
+                  TEAM_COLORS?.[game.awayTeam.name as keyof typeof TEAM_COLORS]
+                    .primary,
+              }}
             >
-              <div
-                style={{ display: "flex", alignItems: "center", gap: "8px" }}
+              <h3
+                className={`text-xl font-bold flex justify-between items-center ${
+                  TEAM_COLORS?.[game.awayTeam.name as keyof typeof TEAM_COLORS]
+                    .text
+                }`}
               >
-                <TeamLogo teamId={game.homeTeam.name?.toLowerCase()} />
-                <span>{game.homeTeam.name}</span>
-              </div>
-              <span
-                title={
-                  game.homeLineupConfirmed
-                    ? "Home lineup confirmed"
-                    : "Home lineup not confirmed"
-                }
-              >
-                {game.homeLineupConfirmed ? "✅" : "❌"}
-              </span>
-            </h3>
+                <div
+                  style={{ display: "flex", alignItems: "center", gap: "8px" }}
+                >
+                  <TeamLogo teamId={game.awayTeam.name?.toLowerCase()} />
+                  <span>{game.awayTeam.name}</span>
+                </div>
+                <span
+                  title={
+                    game.awayLineupConfirmed
+                      ? "Away lineup confirmed"
+                      : "Away lineup not confirmed"
+                  }
+                >
+                  {game.awayLineupConfirmed ? "✅" : "❌"}
+                </span>
+              </h3>
+            </div>
+            <Suspense>
+              <LineupSection
+                teamId={game.awayTeam.id}
+                opposingTeamId={game.homeTeam.id}
+                gameId={game.id}
+              />
+            </Suspense>
           </div>
-          <Suspense>
-            <LineupSection
-              teamId={game.homeTeam.id}
-              opposingTeamId={game.awayTeam.id}
-              gameId={game.id}
-            />
-          </Suspense>
+          <div>
+            <div
+              className="p-4 rounded-t-lg "
+              style={{
+                backgroundColor:
+                  TEAM_COLORS?.[game.homeTeam.name as keyof typeof TEAM_COLORS]
+                    .primary,
+              }}
+            >
+              <h3
+                className={`text-xl font-bold flex justify-between items-center ${
+                  TEAM_COLORS?.[game.homeTeam.name as keyof typeof TEAM_COLORS]
+                    .text
+                }`}
+              >
+                <div
+                  style={{ display: "flex", alignItems: "center", gap: "8px" }}
+                >
+                  <TeamLogo teamId={game.homeTeam.name?.toLowerCase()} />
+                  <span>{game.homeTeam.name}</span>
+                </div>
+                <span
+                  title={
+                    game.homeLineupConfirmed
+                      ? "Home lineup confirmed"
+                      : "Home lineup not confirmed"
+                  }
+                >
+                  {game.homeLineupConfirmed ? "✅" : "❌"}
+                </span>
+              </h3>
+            </div>
+            <Suspense>
+              <LineupSection
+                teamId={game.homeTeam.id}
+                opposingTeamId={game.awayTeam.id}
+                gameId={game.id}
+              />
+            </Suspense>
+          </div>
         </div>
       </div>
     </div>
